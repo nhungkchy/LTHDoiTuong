7c95050b41b1f0eee8e86164f09603f9b272978d
Lê Thị Hồng Nhung <lethihongnhung@MacBook-Pro-cua-Nhung.local>
2020-10-29 19:30:27 +0700

    enum

---
+++ enum_program/Program.cs
@@ -0,0 +1,38 @@
+﻿// enum
+using System;
+
+namespace EnumApplication 
+{
+   class EnumProgram 
+   {
+      enum Days 
+      { 
+          Sun = 8, 
+          Mon = 2, 
+          Tue = 3, 
+          Wed = 4, 
+          Thu = 5, 
+          Fri = 6, 
+          Sat = 7
+        };
+      public enum Color: byte
+      {
+          red = 1,
+          blue = 2,
+          green = 3
+      }
+    
+
+      static void Main(string[] args) 
+      {
+         int weekdayStart = (int)Days.Mon;
+         int weekdayEnd = (int)Days.Fri;
+         var red = Color.red;
+         
+         Console.WriteLine("Monday: {0}", weekdayStart);
+         Console.WriteLine("Friday: {0}", weekdayEnd);
+         Console.WriteLine("Red: {0}", red);
+         Console.ReadKey();
+      }
+   }
+}
---
+++ static_program/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using MyMath;
+
+namespace static_program
+{
+    public static class MyMath
+    {
+        public static int Max(params int[] numbers)
+        {
+            if (numbers.Length == 0)
+            {
+                throw new ArgumentException("Khong co gia tri de so sanh", "numbers");
+            }
+            int max = numbers[0];
+            foreach (var number in numbers)
+            {
+                if(number > max)
+                {
+                    max = number;
+                }
+            }
+            return max;
+        }
+        public static int Min(params int[] numbers)
+        {
+            if(numbers.Length == 0)
+            {
+                throw new ArgumentException("Khong co gia tri de so sanh", " numbers");
+            }
+            int min = numbers[0];
+            foreach(var number in numbers)
+            {
+                if (number < min)
+                {
+                    min = number;
+                }
+            }
+            return min;
+        }
+    }
+    class Program
+    {
+        //         public static int Max(params int[] numbers)
+        // {
+        //     if (numbers.Length == 0)
+        //     {
+        //         throw new ArgumentException("Khong co gia tri de so sanh", "numbers");
+        //     }
+        //     int max = numbers[0];
+        //     foreach (var number in numbers)
+        //     {
+        //         if(number > max)
+        //         {
+        //             max = number;
+        //         }
+        //     }
+        //     return max;
+        // }
+        // public static int Min(params int[] numbers)
+        // {
+        //     if(numbers.Length == 0)
+        //     {
+        //         throw new ArgumentException("Khong co gia tri de so sanh", " numbers");
+        //     }
+        //     int min = numbers[0];
+        //     foreach(var number in numbers)
+        //     {
+        //         if (number < min)
+        //         {
+        //             min = number;
+        //         }
+        //     }
+        //     return min;
+        // }
+        static void Main(string[] args)
+        {
+            int[] numbers = new[] {1,2,7,3,9,30,5,35};
+
+            int max = Max(numbers);
+            int min = Min(numbers);
+
+            Console.WriteLine("Max: {0}", max);
+            Console.WriteLine("Min: {0}", min);
+        }
+    }
+
+}
