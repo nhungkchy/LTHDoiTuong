81ecd495c1fd59c3df25ddc0b702280fb00c8169
Lê Thị Hồng Nhung <lethihongnhung@MacBook-Pro-cua-Nhung.local>
2020-10-29 21:15:36 +0700

    static( static class, static method, static property )

--- static_program/Program.cs
+++ static_program/Program.cs
@@ -1,7 +1,37 @@
-﻿using System;
+﻿// static( static class, static method, static property )
+
+using System;
 
 namespace static_program
 {
+    class Math_Program 
+    {
+        public static int number_one
+        {
+            get{ return number;}
+            private set { number= value;}
+        }
+        public static int number_two
+        {
+            get{ return number;}
+            private set { number = value;}
+        }
+        private static int number = 3;
+        private static double result;
+        public static double square()
+        {
+            result = Math.Pow(number_one, number_two);
+            return result;
+        }
+        public static double average()
+        {
+            result = (number_one + number_two)/2;
+            return result;
+        }
+
+    }
+
+
     public static class MyMath
     {
         public static int Max(params int[] numbers)
@@ -37,10 +67,15 @@ namespace static_program
             return min;
         }
     }
+
+
     class Program
     {
         static void Main(string[] args)
-        {
+        {   
+            // double num;
+            // num = Math_Program.square();
+            // Console.WriteLine(num);
             int[] numbers = new[] {1,2,7,3,9,30,5,35};
 
             int max = MyMath.Max(numbers);
